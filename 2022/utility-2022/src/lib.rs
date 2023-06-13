86a960d492bacdbc2040bd5435b03ec8c59275d1
G00Z_G00Z <edygomezgg4@gmail.com>
2023-06-13 09:56:49 -0400

    Added utility function to read input files

--- 2022/utility-2022/src/lib.rs
+++ 2022/utility-2022/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 pub fn is_demo_mode() -> bool {
     dotenv::dotenv().ok();
 
@@ -6,10 +8,34 @@ pub fn is_demo_mode() -> bool {
     match demo_mode {
         Some(val) => val == "1",
         None => {
-            println!("DEMO_MODE not set, defaulting to false");
+            eprintln!("DEMO_MODE not set, defaulting to false");
             false
         }
     }
+}
+
+const INPUT: &str = "input.txt";
+const DEMO_INPUT: &str = "demo-input.txt";
+
+pub fn get_input() -> String {
+    let is_demo = is_demo_mode();
+
+    if is_demo {
+        println!("Demo mode is enabled");
+    }
+
+    let filename = {
+        if is_demo {
+            DEMO_INPUT
+        } else {
+            INPUT
+        }
+    };
+
+    let input =
+        fs::read_to_string(filename).expect(format!("{} failed to read file", filename).as_str());
+
+    input
 }
 
 #[cfg(test)]
