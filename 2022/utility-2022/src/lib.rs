6d7649ab762cc176268ed01b64ca0c638ad619f8
G00Z_G00Z <edygomezgg4@gmail.com>
2023-06-13 10:11:04 -0400

    Documented utility code

--- 2022/utility-2022/src/lib.rs
+++ 2022/utility-2022/src/lib.rs
@@ -1,5 +1,7 @@
 use std::fs;
 
+/// Checks in the .env file for DEMO_MODE and returns true if it is set to 1
+/// If DEMO_MODE is not set, it will return false but it will print an error message
 pub fn is_demo_mode() -> bool {
     dotenv::dotenv().ok();
 
@@ -17,6 +19,8 @@ pub fn is_demo_mode() -> bool {
 const INPUT: &str = "input.txt";
 const DEMO_INPUT: &str = "demo-input.txt";
 
+/// Reads the input.txt file and returns the contents as a String
+/// If DEMO_MODE is set to 1 in the .env file, it will read the demo-input.txt file instead
 pub fn get_input() -> String {
     let is_demo = is_demo_mode();
 
