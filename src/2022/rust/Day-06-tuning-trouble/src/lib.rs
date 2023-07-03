6d95b601ce0c6e4441ae56c1ba50bb4f208ad1c4
G00Z_G00Z <edygomezgg4@gmail.com>
2023-07-03 10:54:33 -0400

    new cases

--- src/2022/rust/Day-06-tuning-trouble/src/lib.rs
+++ src/2022/rust/Day-06-tuning-trouble/src/lib.rs
@@ -1,7 +1,12 @@
 use std::collections::HashSet;
 
-pub fn detect_sub_routine(signal: &str) -> Option<u32> {
-    for window in signal.chars().enumerate().collect::<Vec<_>>().windows(4) {
+pub fn detect_sub_routine(signal: &str, diff_char_count: usize) -> Option<u32> {
+    for window in signal
+        .chars()
+        .enumerate()
+        .collect::<Vec<_>>()
+        .windows(diff_char_count)
+    {
         let place = window[3].0 + 1;
         let (a, b, c, d) = (window[0].1, window[1].1, window[2].1, window[3].1);
         let set = HashSet::from([a, b, c, d]);
@@ -17,47 +22,90 @@ mod tests {
     use super::*;
     use utility_2022::{get_input, is_demo_mode};
 
-    #[test]
-    fn prueba_demo() {
-        if !is_demo_mode() {
-            println!("Demo mode is not enabled");
-            return;
-        }
+    mod part_1 {
+        use super::*;
 
-        let input = get_input();
+        #[test]
+        fn test_input_demo() {
+            if !is_demo_mode() {
+                println!("Demo mode is not enabled");
+                return;
+            }
 
-        let result = detect_sub_routine(&input).expect("Test case with input must return a number");
+            let input = get_input();
 
-        assert_eq!(result, 7);
-    }
-
-    #[test]
-    fn test_part_1() {
-        if is_demo_mode() {
-            println!("Demo mode is enabled");
-            return;
-        }
-
-        let input = get_input();
-
-        let result = detect_sub_routine(&input).expect("Test case with input must return a number");
-
-        println!("Result pt1: {}", result);
-    }
-
-    #[test]
-    fn prueba_demo_2() {
-        let trials = [
-            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
-            ("nppdvjthqldpwncqszvftbrmjlhg", 6),
-            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
-            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
-        ];
-
-        for t in trials.iter() {
             let result =
-                detect_sub_routine(t.0).expect("Test case with input must return a number");
-            assert_eq!(result, t.1);
+                detect_sub_routine(&input, 4).expect("Test case with input must return a number");
+
+            assert_eq!(result, 7);
+        }
+
+        #[test]
+        fn test_input() {
+            if is_demo_mode() {
+                println!("Demo mode is enabled");
+                return;
+            }
+
+            let input = get_input();
+
+            let result =
+                detect_sub_routine(&input, 4).expect("Test case with input must return a number");
+
+            println!("Result pt1: {}", result);
+        }
+
+        #[test]
+        fn test_cases() {
+            let trials = [
+                ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
+                ("nppdvjthqldpwncqszvftbrmjlhg", 6),
+                ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
+                ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+            ];
+
+            for t in trials.iter() {
+                let result =
+                    detect_sub_routine(&t.0, 4).expect("Test case with input must return a number");
+                assert_eq!(result, t.1);
+            }
+        }
+    }
+
+    mod part_2 {
+
+        use super::*;
+
+        #[test]
+        fn test_cases() {
+            let trials = [
+                ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+                ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+                ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+                ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+                ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+            ];
+
+            for t in trials.iter() {
+                let result = detect_sub_routine(&t.0, 14)
+                    .expect("Test case with input must return a number");
+                assert_eq!(result, t.1, "Failed for {}", t.0);
+            }
+        }
+
+        #[test]
+        fn test_input() {
+            if is_demo_mode() {
+                println!("Demo mode is enabled");
+                return;
+            }
+
+            let input = get_input();
+
+            let result =
+                detect_sub_routine(&input, 14).expect("Test case with input must return a number");
+
+            println!("Result pt1: {}", result);
         }
     }
 }
