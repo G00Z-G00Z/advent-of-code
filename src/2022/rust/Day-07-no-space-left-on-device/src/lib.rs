3c5a3e3e9ae408d87329c6d8ebc76a598c46ed40
G00Z_G00Z <edygomezgg4@gmail.com>
2023-07-05 10:30:27 -0400

    Make filesystem
    Be warned, printing produces stack overflow

--- src/2022/rust/Day-07-no-space-left-on-device/src/lib.rs
+++ src/2022/rust/Day-07-no-space-left-on-device/src/lib.rs
@@ -1,22 +1,38 @@
+use std::{cell::RefCell, rc::Rc};
+
+pub type NodeHanlder<T> = Rc<RefCell<T>>;
+
 /// File representation
-#[derive(Debug, PartialEq)]
-struct File {
+#[derive(Debug)]
+pub struct File {
     pub size: u64,
+    pub name: String,
 }
 
 /// Folder representation
-#[derive(Debug, PartialEq)]
-struct Folder {
+#[derive(Debug)]
+pub struct Folder {
     name: String,
-    children: Vec<Folder>,
+    children: Vec<NodeHanlder<Folder>>,
     files: Vec<File>,
+    parent: Option<NodeHanlder<Folder>>,
 }
 
 impl Folder {
-    fn size(&self) -> u64 {
+    pub fn new_rc(name: String, parent: Option<NodeHanlder<Folder>>) -> NodeHanlder<Folder> {
+        Rc::new(RefCell::new(Folder {
+            name,
+            children: vec![],
+            files: vec![],
+            parent: parent,
+        }))
+    }
+
+    pub fn size(&self) -> u64 {
         let mut size = 0;
         for child in &self.children {
-            size += child.size();
+            let folder = child.borrow();
+            size += folder.size();
         }
         for file in &self.files {
             size += file.size;
@@ -24,37 +40,138 @@ impl Folder {
         size
     }
 
-    fn add_file(&mut self, file: File) {
+    pub fn add_file(&mut self, file: File) {
         self.files.push(file);
     }
 
-    fn add_folder(&mut self, folder: Folder) {
-        self.children.push(folder);
+    pub fn add_folder(&mut self, folder: NodeHanlder<Folder>) {
+        self.children.push(Rc::clone(&folder));
     }
+}
+
+/// Representation of a command
+#[derive(Debug)]
+enum Command {
+    Ls,
+    CdBack,
+    Cd(String),
+}
+
+/// Command representation
+#[derive(Debug)]
+enum ParseResult {
+    Command(Command),
+    File(File),
+    Folder(String),
 }
 
 /// File system representation
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 struct FileSystem {
-    root: Folder,
+    root: NodeHanlder<Folder>,
 }
 
-fn create_file_system_from_cmd(input: &str) -> FileSystem {
-    let mut root = Folder {
-        name: String::from("/"),
-        children: vec![],
-        files: vec![],
-    };
+pub fn create_file_system_from_cmd<'a>(input: &str) -> NodeHanlder<Folder> {
+    let root: NodeHanlder<Folder> = Folder::new_rc("/".into(), None);
 
-    let mut current_folder: &mut Folder;
+    let mut current_folder = Rc::clone(&root);
 
-    current_folder = &mut root;
+    let input = parse_input(&input);
 
-    for line in input.lines() {
-        println!("line: {}", line);
+    let mut c = 0;
+
+    for entry in input {
+        match entry {
+            ParseResult::Command(c) => match c {
+                Command::Ls => {}
+                Command::CdBack => {
+                    let parent = Rc::clone(
+                        current_folder
+                            .borrow()
+                            .parent
+                            .as_ref()
+                            .expect("Cannot go back"),
+                    );
+
+                    current_folder = parent;
+                }
+                Command::Cd(n) => {
+                    let mut tmp = None;
+
+                    for child in &current_folder.borrow().children {
+                        if child.borrow().name == n {
+                            tmp = Some(Rc::clone(child));
+                            break;
+                        }
+                    }
+
+                    if let Some(folder) = tmp {
+                        current_folder = folder;
+                    }
+                }
+            },
+            ParseResult::File(f) => {
+                let mut folder = current_folder.borrow_mut();
+                folder.add_file(f);
+            }
+            ParseResult::Folder(f) => {
+                let folder = Folder::new_rc(f, Some(Rc::clone(&current_folder)));
+                // Aqui es donde muere
+                current_folder.borrow_mut().add_folder(folder);
+            }
+        }
     }
 
-    FileSystem { root }
+    root
+}
+fn parse_input(input: &str) -> Vec<ParseResult> {
+    let mut results = vec![];
+
+    for line in input.lines().skip(1) {
+        if line.starts_with("$") {
+            let mut splits = line.split(" ").skip(1);
+            let command_str = splits.next().unwrap();
+
+            match command_str {
+                "ls" => {
+                    results.push(ParseResult::Command(Command::Ls));
+                }
+                "cd" => {
+                    let path = splits.next().unwrap();
+
+                    if path == ".." {
+                        results.push(ParseResult::Command(Command::CdBack));
+                        continue;
+                    }
+
+                    results.push(ParseResult::Command(Command::Cd(String::from(path))));
+                }
+                _ => {
+                    panic!("Unknown command");
+                }
+            }
+            continue;
+        }
+
+        let mut splits = line.split(" ");
+
+        match splits.next().unwrap() {
+            "dir" => {
+                let name = splits.next().unwrap().to_string();
+                results.push(ParseResult::Folder(name));
+            }
+            number => {
+                let name = splits.next().unwrap().to_string();
+                let size = number
+                    .parse::<u64>()
+                    .expect(&format!("Cannot parse number {}", number));
+
+                results.push(ParseResult::File(File { name, size }));
+            }
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -85,45 +202,22 @@ mod tests {
             return;
         }
 
-        let expected_filesystem = FileSystem {
-            root: Folder {
-                name: String::from("/"),
-                children: vec![
-                    Folder {
-                        name: String::from("a"),
-                        children: vec![Folder {
-                            name: String::from("e"),
-                            children: vec![],
-                            files: vec![File { size: 584 }],
-                        }],
-                        files: vec![
-                            File { size: 29116 },
-                            File { size: 2557 },
-                            File { size: 62596 },
-                        ],
-                    },
-                    Folder {
-                        name: String::from("d"),
-                        children: vec![],
-                        files: vec![
-                            File { size: 4060174 },
-                            File { size: 8033020 },
-                            File { size: 5626152 },
-                            File { size: 7214296 },
-                        ],
-                    },
-                ],
-                files: vec![File { size: 14848514 }, File { size: 8504156 }],
-            },
-        };
+        let expected_size = 48381165;
 
         let demo_input_file_system: String = get_input();
-        let demo_input_file_system: FileSystem =
-            create_file_system_from_cmd(&demo_input_file_system);
+
+        let root = RefCell::new(Box::new(Folder {
+            name: String::from("/"),
+            children: vec![],
+            files: vec![],
+            parent: None,
+        }));
+
+        let root = create_file_system_from_cmd(&demo_input_file_system);
 
         assert_eq!(
-            demo_input_file_system.root.size(),
-            expected_filesystem.root.size(),
+            root.borrow().size(),
+            expected_size,
             "Size calculation do not match"
         );
     }
