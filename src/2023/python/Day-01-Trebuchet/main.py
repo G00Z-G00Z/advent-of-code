dfca909ff8ba9699b980a0f7b798d5b93ef8ccb1
G00Z_G00Z <edygomezgg4@gmail.com>
2023-12-15 12:35:28 -0600

    first day solution python

--- src/2023/python/Day-01-Trebuchet/main.py
+++ src/2023/python/Day-01-Trebuchet/main.py
@@ -13,9 +13,38 @@ def read_file(file_path: str) -> str:
         return f.read()
 
 
+def sol_1(content: str) -> int:
+    sum: int = 0
+    first: int = 0
+    last: int = 0
+
+    for line in content.split("\n"):
+        if not line:
+            continue
+        # Look number from the front
+        print(line)
+        for i in range(0, len(line) - 1):
+            if line[i].isdigit():
+                first = int(line[i])
+                break
+
+        # Look number from the back
+        for i in range(len(line) - 1, 0, -1):
+            if line[i].isdigit():
+                last = int(line[i])
+                break
+
+        number = first * 10 + last
+        print(f"Found number {number}")
+        sum += number
+
+    return sum
+
+
 def main():
     content = read_file(str(FILE_PATH))
-    print(content)
+    sum = sol_1(content)
+    print(f"Sum: {sum}")
 
 
 if __name__ == "__main__":
